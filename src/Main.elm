5e51ecf0607a618b9c59d9ba9cff27c0173e57bf
Marco Wahl <marcowahlsoft@gmail.com>
2016-11-11 12:50:35 +0100

    ~:content

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,36 @@
-import Html exposing ( Html, button, div, text, h1, body
+import Html exposing ( Html, button, div, text, h1, h2, body
                      , span, p, ol, ul, li)
 import Html.App as App
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (class, style)
 
-green_style =  style [("color", "green")]
+green_style = style [("color", "green")]
+red_style =  style [("color", "red")]
 
-main = body   []
+main = body []
        [ div [ class "centered" ]
-             [ h1  [] [ text "Ent"
-                     , span [ green_style ] [text "zifferer" ]]
-             , p  [] [ span [ green_style ] [text "Sie geben uns"]
+             [ text "Unsere Agebote"
+             , h2  [] [ text "Ent"
+                     , span [ green_style ] [text "zifferor" ]]
+             , p  [] [ span [ red_style ] [text "Sie geben uns"]
                     , ul []
                         [ li [] [text "Handgeschriebenes"]
-                        , li [] [text "Geld" ]
                         ]
                     ]
              , p  [] [ span [ green_style ] [ text "Wir geben Ihnen"]
                     , ul []
                         [ li [] [ text "Eine Abschrift Ihres Dokuments in elektronischer Form" ] ]
+                    ]
+             , h2  [] [ text "Korrektor" ]
+             , p  [] [ text "Korrektur von Abschlussarbeiten." ]
+             , p  [] [ span [ red_style ] [text "Sie geben uns"]
+                    , ul []
+                        [ li [] [text "Abschlussarbeit in elektronischer Form"]
+                        ]
+                    ]
+             , p  [] [ span [ green_style ] [ text "Wir geben Ihnen"]
+                    , ul []
+                        [ li [] [ text "Korrekturvorschläge in elektronischer Form" ] ]
                     ]
              ]
        ]
